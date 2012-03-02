21ceb5acf3ea28e15e20ba55b28025b66474dfd3
Chris Wilcox <topher515@gmail.com>
2012-03-02 01:54:25 -0800

    Start work on tnt page

---
+++ teamintraining/2012/index.php
@@ -0,0 +1,121 @@
+<html>
+<head>
+    <title>Donate to Leukemia &amp; Lymphoma Society | Chris Wilcox</title>
+    <style>
+    body {
+        margin:0;
+        background-color:rgb(231,235,242);
+    }
+    .l-content {
+        width:55%;
+        top:200px;
+        position:absolute;
+        left:0;
+        text-align:center;
+    }
+    .r-content {
+        text-align:center;
+        right:0;
+        top:0;
+        width:40%;
+        position:absolute;
+    }
+    .title {
+        background-image:url(title.jpg);
+        background-size:auto 100px;
+        background-repeat:no-repeat;
+        height:100px;
+        position:absolute;
+        left:0;
+        right:0;
+        top:50px;
+        background-color:rgb(146,100,172);
+        z-index:-1;
+    }
+    .last-year {
+        position:absolute;
+        top:150px;
+        right:35%;
+        z-index:100;
+        width:280px;
+    }
+    .btn-lls {
+      background-color: hsl(274, 31%, 46%) !important;
+      background-repeat: repeat-x;
+      filter: e(%("progid:DXImageTransform.Microsoft.gradient(startColorstr='%d', endColorstr='%d', GradientType=0)", #b094c6, #7a5099));
+      background-image: -khtml-gradient(linear, left top, left bottom, from(#b094c6), to(#7a5099));
+      background-image: -moz-linear-gradient(top, #b094c6, #7a5099);
+      background-image: -ms-linear-gradient(top, #b094c6, #7a5099);
+      background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #b094c6), color-stop(100%, #7a5099));
+      background-image: -webkit-linear-gradient(top, #b094c6, #7a5099);
+      background-image: -o-linear-gradient(top, #b094c6, #7a5099);
+      background-image: linear-gradient(#b094c6, #7a5099);
+      border-color: #7a5099 #7a5099 hsl(274, 31%, 40.5%);
+      color: #FFF !important;
+      text-shadow: 0 1px 1px rgba(255, 255, 255, 0.36);
+      -webkit-font-smoothing: antialiased;
+      border-radius: 5px;
+      padding: 10px 20px;
+      font-size: 20px;
+      cursor:pointer;
+    }
+    .tnt-images img {
+        max-width:80%;
+        border:10px white solid;
+        margin:10px;
+        outline:1px #ccc solid;
+    }
+    .tnt-images .sep {
+        width:10px;
+        height:25px;
+        background-color:white;
+        display:block;
+        margin:auto;
+        outline:1px #ccc solid;
+    }
+    
+    </style>
+</head>
+<body>
+    <div class="title"></div>
+    
+    <img class="last-year" src="last_year.png" />
+        
+    <div class="l-content">
+        <form id="ProcessDonation" name="ProcessDonation" action="http://pages.teamintraining.org//ProcessDonation" target="formTarget1330677525075" method="post">
+            <!-- input type="radio" value="0" alt="Other" id="donateAmountId" name="donateAmount" class="Radio_Group">&nbsp;Other $ 
+            <input type="text" alt="OtherAmount" name="enteredAmount" class="otherfieldcell" maxlength="15"><br -->
+            <input type="hidden" name="fundraiserPageID" value="855574" id="ProcessDonation_fundraiserPageID">
+            <input type="hidden" name="participantFirstName" value="Chris" id="ProcessDonation_participantFirstName">
+            <input type="hidden" name="participantLastName" value="Wilcox" id="ProcessDonation_participantLastName">
+            <input type="hidden" name="displayNameURL" value="Chris+Wilcox" id="ProcessDonation_displayNameURL">
+            <input type="submit" id="ProcessDonation_0"  class="btn-lls registerButton submit" value="Donate to LLS" />
+            <p>You'll be taken to the Leukemia &amp; Lymphomia Society's Official Donation page</p>
+        </form>
+        
+    </div>
+        
+    <div class="r-content">    
+        
+        <div class="tnt-images">
+            <img src="bk_bike.jpeg" alt="Bk Bike">
+            <div class="sep"></div>
+            <img src="bk_swim_finish.jpeg" alt="Bk Swim Finish">
+            <div class="sep"></div>
+            <img src="bk_water_stand.jpeg" alt="Bk Water Stand">
+            <div class="sep"></div>
+            <img src="bk_wetsuit_wave.jpeg" alt="Bk Wetsuit Wave">
+            <div class="sep"></div>
+            <img src="finish_strong.jpeg" alt="Finish Strong">
+            <div class="sep"></div>
+            <img src="race_prep.jpeg"alt="Race Prep">
+            <div class="sep"></div>
+            <img src="run.jpeg" alt="Run">
+            <div class="sep"></div>
+            <img src="strong_wetsuits.jpeg" alt="Strong Wetsuits">
+        </div>
+                
+    </div>
+
+</body>
+</html>
